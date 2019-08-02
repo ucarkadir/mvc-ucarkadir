6f00ec856384971f11df51d3fbd6c454452548b1
kadir.ucar <kadir.ucar@dbhsoft.com>
2019-08-02 10:12:16 +0300

    cat view

---
+++ ucarkadir/Models/CatView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ucarkadir.Models
+{
+    public class CatView
+    {
+        public Cat tCat { get; set; }
+    }
+}
